2eefa2b2c82dda8a980c84ef0442df3a9523ca78
Ben Burwell <bburwell1@gmail.com>
2015-03-12 13:19:00 -0400

    update rule

--- rule.h
+++ rule.h
@@ -1,12 +1,11 @@
 enum { DENY, ALLOW } mfw_rule_action;
-enum { IN, OUT } mfw_rule_direction;
-enum { TCP, UDP, ALL } mfw_rule_protocol;
+enum { ALL, IN, OUT } mfw_rule_direction;
 
 
 struct mfw_rule {
     enum mfw_rule_action action;
     enum mfw_rule_direction direction;
-    enum mfw_rule_protocol protocol;
+    uint8_t protocol;
     uint8_t srcport;
     uint8_t destport;
 };
