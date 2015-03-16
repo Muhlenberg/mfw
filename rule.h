4f2177e78233da4306003d999fdd919dcd32cbdf
Holly Bertolet <hbertolet@gmail.com>
2015-03-16 18:57:50 -0400

    Update rule.h

--- rule.h
+++ rule.h
@@ -1,14 +1,12 @@
 #include <stdint.h>
 
-enum { DENY, ALLOW } mfw_rule_action;
-enum { IN, OUT } mfw_rule_direction;
-enum { TCP, UDP, ALL } mfw_rule_protocol;
-
+enum mfw_rule_action { DENY, ALLOW };
+enum mfw_rule_direction { IN, OUT };
 
 struct mfw_rule {
     enum mfw_rule_action action;
     enum mfw_rule_direction direction;
-    enum mfw_rule_protocol protocol;
+    int8_t protocol;
     uint8_t srcport;
     uint8_t destport;
 };
