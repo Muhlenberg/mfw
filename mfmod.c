19ea64325782d68b964db4140bbf423745d860f3
Ben Burwell <ben@benburwell.com>
2015-03-27 09:56:45 -0400

    start working on procfile

--- mfmod.c
+++ mfmod.c
@@ -2,8 +2,39 @@
 #include <linux/kernel.h>
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
+#include <linux/fs.h>
+#include <linux/proc_fs.h>
+
+#define PROC_FS_MAX 1024
 
 static struct nf_hook_ops nfho;
+static struct proc_dir_entry *rule_file;
+static char procfs_buffer[PROC_FS_MAX];
+static int proc_buffer_size = 0;
+
+ssize_t read_callback (struct file *fp, char *d, size_t len, loff_t *l) {
+		return sizeof(char);
+}
+
+ssize_t write_callback (struct file *fp, const char *d, size_t len, loff_t *l) {
+	return sizeof(char);
+}
+
+int open_callback (struct inode *n, struct file *fp) {
+	return 0;
+}
+
+int release_callback (struct inode *n, struct file *fp) {
+	return 0;
+}
+
+static const struct file_operations proc_file_fops = {
+	.owner = THIS_MODULE,
+	.open = open_callback,
+	.read = read_callback,
+	.write = write_callback,
+	.release = release_callback,
+};
 
 unsigned int hook_func(unsigned int hooknum,
 	struct sk_buff *skb,
@@ -19,7 +50,7 @@ unsigned int hook_func(unsigned int hooknum,
         struct mfw_rule* rule = rules[i];
         int i=0;
         for (i; i < rules.length; i++) {
-            
+
             ip_header = (stuct iphdr *)skb_network_header(socket_buff); // Network header
 
             // If the packet's protocol matches the rule
